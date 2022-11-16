7748e2d025915cc56016d13d71ca7a4fffdd7ee4
choongang2022 <choongang2022@gmail.com>
2022-11-16 09:49:04 +0900

    first commit

---
+++ choongangGroup/src/main/java/com/oracle/choongangGroup/ChoongangGroupApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.choongangGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoongangGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoongangGroupApplication.class, args);
+	}
+
+}
---
+++ choongangGroup/src/test/java/com/oracle/choongangGroup/ChoongangGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.choongangGroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoongangGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
