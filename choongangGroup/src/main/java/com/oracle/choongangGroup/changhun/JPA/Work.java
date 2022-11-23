f5bb8d045c68e22f4e8ffba32aa6ae76ff655013
choongang2022 <choongang2022@gmail.com>
2022-11-23 16:30:23 +0900

    근태관리 테이블 추가

---
+++ choongangGroup/src/main/java/com/oracle/choongangGroup/changhun/JPA/Work.java
@@ -0,0 +1,39 @@
+package com.oracle.choongangGroup.changhun.JPA;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+@IdClass(WorkPK.class)
+public class Work {
+	
+	@Id
+	@Column(name = "work_date")
+	private String workDate;
+	
+	@Id
+	private String userid;
+	
+	@ManyToOne(fetch = FetchType.LAZY,
+			   cascade = CascadeType.ALL)
+	@JoinColumn(name = "userid", insertable = false, updatable = false)
+	private Member member;
+	private String attOnTime;
+	private String sttOffTime;
+	private Long vacation;
+	
+	@Enumerated(EnumType.STRING)
+	private WorkStatus attStatus;
+	
+}
