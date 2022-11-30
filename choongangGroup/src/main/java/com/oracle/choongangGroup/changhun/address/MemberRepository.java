b382f79d007db483b1e06af457748daac81da08a
user <user@DESKTOP-26RME5F>
2022-11-30 10:31:27 +0900

    현지오류수정

--- choongangGroup/src/main/java/com/oracle/choongangGroup/changhun/address/MemberRepository.java
+++ choongangGroup/src/main/java/com/oracle/choongangGroup/changhun/address/MemberRepository.java
@@ -1,13 +1,10 @@
 package com.oracle.choongangGroup.changhun.address;
 
 
-<<<<<<< HEAD
 
-
-=======
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
->>>>>>> branch 'hyeonji' of https://github.com/hyeonji22/team-project.git
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.oracle.choongangGroup.changhun.JPA.Member;
@@ -18,10 +15,8 @@ public interface MemberRepository extends JpaRepository<Member, String>{
 
 	Member findByUserid(String userid);
 
-<<<<<<< HEAD
-=======
+
 	Page<Member> findByNameContaining(String name, PageRequest of);
 
->>>>>>> branch 'hyeonji' of https://github.com/hyeonji22/team-project.git
 
 }
