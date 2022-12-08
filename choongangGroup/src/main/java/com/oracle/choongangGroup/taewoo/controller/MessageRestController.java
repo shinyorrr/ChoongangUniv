a233527286e6c1030285d541befb33fa7d12f5e4
taewoooh <taewoooh@oreoWoo>
2022-12-08 17:55:40 +0900

    [TW] 쪽지 주소록 구현

--- choongangGroup/src/main/java/com/oracle/choongangGroup/taewoo/controller/MessageRestController.java
+++ choongangGroup/src/main/java/com/oracle/choongangGroup/taewoo/controller/MessageRestController.java
@@ -2,12 +2,21 @@ package com.oracle.choongangGroup.taewoo.controller;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oracle.choongangGroup.changhun.JPA.Dept;
 import com.oracle.choongangGroup.changhun.JPA.Member;
 import com.oracle.choongangGroup.changhun.address.MemberRepository;
+import com.oracle.choongangGroup.hj.service.MemberService;
+import com.oracle.choongangGroup.hs.approval.MemDept;
+
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,16 +25,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MessageRestController {
 	
-	public final MemberRepository memberRepository;
+	private final  MemberRepository memberRepository;
+		
+//	@RequestMapping("/addressList")
+//	public List<CreateMemberResponse> addressForm() {
+//		System.out.println("addressForm start....");
+//		log.info("addressForm start....");
+//		List<MemDept> memberList = memberRepository.findAll();
+//		List<CreateMemberResponse> memberCollect = memberList.stream()
+//				  .map(m -> new CreateMemberResponse(m.getDept(), m.getName() ))	// 람다식 표현
+//				  .collect(Collectors.toList());
+//
+////		for(Member member : memberList) {
+////			System.out.println("member->"+member);
+////		}
+//		memberList.get(0).getName();
+//		System.out.println(memberList.get(0).getName());
+//		System.out.println("memberList.size()->"+memberList.size());
+//		return memberCollect;
+//	}
+//	
+//	@Data
+//	@RequiredArgsConstructor
+//	class CreateMemberResponse {
+//		private  final String dname;
+//		private final String name;
+//
+////		public CreateMemberResponse(Long id) {
+////		this.id = id;
+////		}
+//	}
 	
-	@RequestMapping(value = "addressList")
-	public List<Member> addressForm() {
-		log.info("addressForm start....");
-		List<Member> memberList = memberRepository.findAll();
-		return memberList;
-	}
-	
-	
-
 
 }
