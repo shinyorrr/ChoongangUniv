0ab528d08be96beab815b2bcf87a8e944ff62c6f
taewoooh <taewoooh@oreoWoo>
2022-12-12 14:33:18 +0900

    [TW] Main 수정, 쪽지 입력, 조회

--- choongangGroup/src/main/java/com/oracle/choongangGroup/taewoo/dto/MessageDto.java
+++ choongangGroup/src/main/java/com/oracle/choongangGroup/taewoo/dto/MessageDto.java
@@ -1,5 +1,6 @@
-package com.oracle.choongangGroup.taewoo.dto;
+ package com.oracle.choongangGroup.taewoo.dto;
 
+import com.oracle.choongangGroup.taewoo.domain.BaseTimeEntity;
 import com.oracle.choongangGroup.taewoo.domain.Message;
 
 import lombok.AllArgsConstructor;
@@ -9,18 +10,20 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MessageDto {
+public class MessageDto extends BaseTimeEntity {
 	private String messageTitle;
 	private String messageContent;
+	private String senderUserid;
 	private String senderName;
-	private String receiverName;
+	private String receiverUserid;
 	
 	public static MessageDto toDto(Message message) {
 		return new MessageDto(
 				message.getMessageTitle(),
 				message.getMessageContent(),
-				message.getSender().getName(),
-				message.getReceiver().getName()				
+				message.getSender().getUserid(),
+				message.getSenderName(),
+				message.getReceiver().getUserid()				
 				);
 	}
 }
